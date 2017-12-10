56213cc021fc69d26f14efb953af3c095bc5038f
sergioclemente <sergioclemente@gmail.com>
2017-12-10 07:55:27 -0800

    Adding macros
    Preliminary version. Macros are hardcoded for now.

--- app/workout_input.tsx
+++ app/workout_input.tsx
@@ -5,6 +5,7 @@ import * as UI from '../ui';
 import * as Model from '../model';
 import Select from './select';
 import SelectOption from './select_option';
+import { SportType } from '../model';
 
 export default class WorkoutInput extends React.Component<any, any> {
 	constructor(props: any) {
@@ -78,7 +79,97 @@ export default class WorkoutInput extends React.Component<any, any> {
 		this._loadWorkout();
 	}
 
+	// BEGIN: Make this more generic
+	rand(min: number, max: number) : number {
+		return Math.floor(Math.random() * (max - min) + min);
+	}
+
+	randElement(array : any) : string {
+		if (array.length > 0) {
+			return array[this.rand(0, array.length)] + "\n";
+		} else {
+			return "";
+		}
+	}
+
+	// NOT USED FOR NOW
+	shuffleInPlace<T>(array: T[]): T[] {
+		// if it's 1 or 0 items, just return
+		if (array.length <= 1) return array;
+	  
+		// For each index in array
+		for (let i = 0; i < array.length; i++) {
+	  
+		  // choose a random not-yet-placed item to place there
+		  // must be an item AFTER the current item, because the stuff
+		  // before has all already been placed
+		  const randomChoiceIndex = this.rand(i, array.length);
+	  
+		  // place our random choice in the spot by swapping
+		  [array[i], array[randomChoiceIndex]] = [array[randomChoiceIndex], array[i]];
+		}
+	  
+		return array;
+	  }
+
+	preProcess(text : string) : string {
+		let warmup_text = "";
+		let warmup_groups = [];
+		if (this.getSportType() == SportType.Bike) {
+			warmup_groups = [
+				// 9 min (warmup)
+				[
+					"(3min, 55), (3min, 65), (3min, 75)",
+					"(9min, 55, 75)"
+				],
+				// 4 min (drill)
+				[
+					"2[(45s, 45, Single leg - left), (15s, 45, both), (45s, 45, Single leg - right), (15s, 45, both)]",
+					"8[(15s, 55, spin ups), (15s, 55)]",
+					"4[(30s, cadence 80/90/100/110, 55), (30s, 55)]"
+				],
+				// 4 min (build)
+				[
+					"4[(15s, *, build), (45s, 55)]",
+					"4[(5s, *, MAX), (55s, 55)]",
+					"4[(45s, 75, 100), (15s, 55)]",
+					"4[(30s, 85, 90, 95, 100), (30s, 55)]"
+				],
+				// static (3min)
+				["(3min, 55)"]
+			];
+		} else if (this.getSportType() == SportType.Run) {
+			warmup_groups = [
+				[
+					"3[(10s, 0, arm swings)]",	
+					"3[(10s, 0, high knees)]",
+					"3[(10s, 0, ham kicks)]",
+					"3[(10s, 0, a-skips)]",
+					"3[(10s, 0, crossover side to sides)]",
+				],
+				[
+					"3[(10s, 0, 10 lunges - 5 each side)]",
+					"3[(10s, 0, 10 reverse lunges - 5 each side)]",
+					"3[(10s, 0, 10 lunges with rotation - 5 each side)]",
+					"3[(10s, 0, sumo squat)]",
+				]				
+			];
+		}
+		for (let i = 0; i < warmup_groups.length; i++) {
+			warmup_text += this.randElement(warmup_groups[i]);
+		}
+
+		if (warmup_text.length > 0) {
+			return text.replace("#wu", warmup_text);
+		} else {
+			return text;
+		}
+	}
+	// END: Make this more generic
+	
 	_onWorkoutTextChange(e) {
+		var workoutText = this.refs['workout_text'] as HTMLTextAreaElement;
+		workoutText.value = this.preProcess(workoutText.value);		
 		this._loadWorkout();
 	}
 
