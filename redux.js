388676c02619b57269ef7bdba7dfda3b2dffcf16
sergioclemente <sergioclemente@gmail.com>
2018-02-26 09:32:55 -0800

    Update redux samples

--- redux.js
+++ redux.js
@@ -1,45 +1,46 @@
-import { applyMiddleware, createStore } from "redux"
-import { combineReducers } from "redux"
-import logger from "redux-logger"
-import thunk from "redux-thunk"
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider, connect } from 'react-redux'
-
-const reducerValue = (state=0, action) => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// node_modules/webpack/bin/webpack.js  redux.js --output-filename redux.min.js
+const redux_1 = require("redux");
+const redux_2 = require("redux");
+const redux_logger_1 = require("redux-logger");
+const redux_thunk_1 = require("redux-thunk");
+const React = require("react");
+const ReactDOM = require("react-dom");
+const react_redux_1 = require("react-redux");
+const reducerValue = (state = 0, action) => {
     if (action.type === "INCREMENT") {
         return state + action.payload;
     }
+    else if (action.type === "DECREMENT") {
+        return state - action.payload;
+    }
     return state;
-}
-
-const reducer = combineReducers({
-  value: reducerValue,
-})
-
-const middleware = applyMiddleware(thunk, logger);
-const store = createStore(reducer, middleware);
-
-store.dispatch((dispatch) =>{
-    dispatch({type: "INCREMENT", payload: 10});
+};
+const reducer = redux_2.combineReducers({
+    value: reducerValue,
 });
-
-
-// class Layout extends React.Component {
-//   componentWillMount() {
-//   }
-
-//   render() {
-//     const { counter } = this.props;
-
-//     return <div>
-//       <h1>Counter</h1>
-//       <ul>{counter}</ul>
-//     </div>
-//   }
-// }
-
+const middleware = redux_1.applyMiddleware(redux_thunk_1.default, redux_logger_1.default);
+const store = redux_1.createStore(reducer, middleware);
+store.dispatch((dispatch) => {
+    dispatch({ type: "INCREMENT", payload: 30 });
+});
+store.dispatch((dispatch) => {
+    dispatch({ type: "DECREMENT", payload: 10 });
+});
+var mapStateToProps = function (state) {
+    console.log("MapStateToProps");
+    return {
+        counter: state.value
+    };
+};
+let Layout = ({ counter }) => {
+    return (React.createElement("div", null,
+        React.createElement("h1", null, "Counter"),
+        React.createElement("ul", null, counter)));
+};
+var LayoutContainer = react_redux_1.connect(mapStateToProps)(Layout);
 const app = document.getElementById('app');
-ReactDOM.render(<Provider store={store}>
-  <Layout />
-</Provider>, app);
+console.log(app);
+ReactDOM.render(React.createElement(react_redux_1.Provider, { store: store },
+    React.createElement(LayoutContainer, null)), app);
---
+++ redux.tsx
@@ -0,0 +1,53 @@
+// node_modules/webpack/bin/webpack.js  redux.js --output-filename redux.min.js
+import { applyMiddleware, createStore } from "redux"
+import { combineReducers } from "redux"
+import logger from "redux-logger"
+import thunk from "redux-thunk"
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import { Provider, connect } from 'react-redux'
+
+const reducerValue = (state=0, action) => {
+    if (action.type === "INCREMENT") {
+        return state + action.payload;
+    } else if (action.type === "DECREMENT") {
+        return state - action.payload;
+    }
+    return state;
+}
+
+const reducer = combineReducers({
+  value: reducerValue,
+})
+
+const middleware = applyMiddleware(thunk, logger);
+const store = createStore(reducer, middleware);
+
+store.dispatch((dispatch) =>{
+    dispatch({type: "INCREMENT", payload: 30});
+});
+
+store.dispatch((dispatch) =>{
+    dispatch({type: "DECREMENT", payload: 10});
+});
+
+var mapStateToProps = function(state) {
+    return {
+        counter: state.value
+    }
+}
+
+let Layout = ({counter}) => {
+    return (<div>
+        <h1>Counter</h1>
+        <ul>{counter}</ul>
+    </div>);
+}
+
+var LayoutContainer = connect(mapStateToProps)(Layout);
+
+const app = document.getElementById('app');
+console.log(app);
+ReactDOM.render(<Provider store={store}>
+  <LayoutContainer />
+</Provider>, app);
