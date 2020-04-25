3af359f193d4a337b47070f18dad6eb006814154
Angelo Ribeiro <aribeiro2@snapchat.com>
2020-04-24 18:10:20 -0700

    Adding pace and updating tests

--- tests.ts
+++ tests.ts
@@ -139,7 +139,7 @@ describe('File Generation', function () {
 	</tags>
 	<workout>
 		<Warmup Duration="600" PowerLow="0.55" PowerHigh="0.75"/>
-		<SteadyState Duration="3600" Power="0.8">
+		<SteadyState Duration="3600" Power="0.8" Pace="0">
 			<textevent timeoffset="0" message="1hr @ 80%"/>
 		</SteadyState>
 		<Cooldown Duration="300" PowerLow="0.75" PowerHigh="0.55"/>
@@ -159,7 +159,7 @@ describe('File Generation', function () {
 	</tags>
 	<workout>
 		<Warmup Duration="600" PowerLow="0.55" PowerHigh="0.75"/>
-		<FreeRide Duration="3600">
+		<FreeRide Duration="3600" Pace="0">
 			<textevent timeoffset="0" message="Free"/>
 		</FreeRide>
 		<Cooldown Duration="300" PowerLow="0.75" PowerHigh="0.55"/>
