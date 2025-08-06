ae554e7c5f6f37e3353f32edb4256335af3e4caa
Sergio <sergioclemente@gmail.com>
2025-08-06 15:59:48 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 rm -rf dist/
 
-node_modules/typescript/bin/tsc -p tsconfig.json
+npx tsc -p tsconfig.json
 if [[ "$?" != 0 ]]; then
 	echo "Build error." 1>&2
 	exit 1
 fi
 echo Compiled TSC files
 
-node_modules/mocha/bin/mocha dist/test/tests.js
+npx mocha dist/test/tests.js
 if [[ "$?" != 0 ]]; then
 	echo "Build error." 1>&2
 	exit 1
@@ -17,7 +17,7 @@ fi
 echo Ran Tests
 
 # Bundle now
-node_modules/webpack/bin/webpack.js --config webpack.config.js
+npx webpack --config webpack.config.js
 if [[ "$?" != 0 ]]; then
 	echo "Build error." 1>&2
 	exit 1
